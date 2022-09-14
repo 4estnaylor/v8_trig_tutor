272def7919342a2c7b7372256ac4f600db097504
4estnaylor <4estnaylor@gmail.com>
2022-09-13 21:42:43 -0400

    fixed env stuff

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by ediing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
