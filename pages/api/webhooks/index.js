1759d8cece86a3444a733cd1f95362012608f534
4estnaylor <4estnaylor@gmail.com>
2022-09-14 20:37:40 -0400

    webhook getting succesfull checkout session

---
+++ pages/api/webhooks/index.js
@@ -0,0 +1,53 @@
+// import Cors from 'micro-cors';
+
+// const cors = Cors({
+//   allowMethods: ['POST', 'HEAD'],
+// });
+
+// // Partial of ./pages/api/webhooks/index.ts
+// // ...
+// const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+// // Stripe requires the raw body to construct the event.
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+export default function webhookHandler(req, res) {
+  if (req.method === 'POST') {
+    console.log('happening');
+    console.log(req.body.data);
+    res.status(200).send();
+  }
+
+  // if (req.method === 'POST') {
+  //   const buf = await buffer(req);
+  //   const sig = req.headers['stripe-signature'];
+
+  //   let event;
+
+  //   try {
+  //     event = stripe.webhooks.constructEvent(
+  //       buf.toString(),
+  //       sig,
+  //       webhookSecret
+  //     );
+  //     console.log(event);
+  //   } catch (err) {
+  //     // On error, log and return the error message
+  //     console.log(`❌ Error message: ${err.message}`);
+  //     res.status(400).send(`Webhook Error: ${err.message}`);
+  //     return;
+  //   }
+
+  //   // Successfully constructed event
+  //   console.log('✅ Success:', event.id);
+  //   res.status(200).send();
+  //   // ...
+  // }
+}
+
+// ...
+// export default cors(webhookHandler);
