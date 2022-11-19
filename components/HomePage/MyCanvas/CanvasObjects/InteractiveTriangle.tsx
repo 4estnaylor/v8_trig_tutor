b658ee8312641d32765979ed2fd313c9424f42ed
4estnaylor <4estnaylor@gmail.com>
2022-11-18 18:17:23 -0600

    working on degrees intro

--- components/HomePage/MyCanvas/CanvasObjects/InteractiveTriangle.tsx
+++ components/HomePage/MyCanvas/CanvasObjects/InteractiveTriangle.tsx
@@ -46,9 +46,7 @@ class InteractiveTriangle {
 
   draw = () => {
     this.update();
-    this.drawAngle(this.pointA, 0);
-    this.drawAngle(this.pointB, 0);
-    this.drawAngle(this.pointC, 0);
+    this.drawAngles();
     this.drawSides();
     this.drawPoints();
   };
@@ -80,7 +78,92 @@ class InteractiveTriangle {
     context.beginPath();
   };
 
-  drawAngles = (angleInRadians: number, x: number, y: number) => {};
+  drawAngles = () => {
+    this.drawAngle(this.pointA);
+    this.drawAngle(this.pointB);
+    this.drawAngle(this.pointC);
+  };
+
+  drawAngle = (point: Point, type: 'internal' | 'external' = 'internal') => {
+    let context = point.context;
+    let radius = this.getShortestSide() / 3;
+    if (radius < point.radius) {
+      radius = point.radius;
+    }
+
+    let allPoints = [this.pointA, this.pointB, this.pointC];
+
+    let otherPoints = allPoints.filter((arrayPoint) => arrayPoint !== point);
+    let [point1, point2] = otherPoints;
+
+    let { dx: dx1, dy: dy1 } = this.get_dx_dy(
+      point.x,
+      point.y,
+      point1.x,
+      point1.y
+    );
+
+    let { dx: dx2, dy: dy2 } = this.get_dx_dy(
+      point.x,
+      point.y,
+      point2.x,
+      point2.y
+    );
+
+    let endpoint1 = { x: point.x + dx1 * radius, y: point.y + dy1 * radius };
+    let endpoint2 = { x: point.x + dx2 * radius, y: point.y + dy2 * radius };
+
+    let angleStart = Math.atan2(dy1, dx1) + 2 * Math.PI;
+    let angleEnd = Math.atan2(dy2, dx2) + 2 * Math.PI;
+
+    if (angleEnd < angleStart) {
+      [angleStart, angleEnd] = [angleEnd, angleStart];
+    }
+
+    context.fillStyle = point.color;
+    context.strokeStyle = point.color;
+    context.lineWidth = 5;
+
+    context.beginPath();
+    if (point === this.pointA) {
+      console.log('anglestart', angleStart);
+      console.log('angleend', angleEnd);
+    }
+
+    let condition;
+
+    if (type === 'internal') {
+      condition = true;
+    } else {
+      condition = false;
+    }
+
+    if (angleEnd - angleStart > Math.PI) {
+      context.arc(point.x, point.y, radius, angleStart, angleEnd, condition);
+    } else {
+      context.arc(point.x, point.y, radius, angleStart, angleEnd, !condition);
+    }
+
+    context.stroke();
+
+    context.lineTo(point.x, point.y);
+    context.closePath();
+
+    context.lineWidth = 3;
+
+    context.globalAlpha = this.angleTransparency;
+    // context.fill();
+    context.globalAlpha = 1;
+    context.beginPath();
+
+    // context.fillRect(endpoint1.x, endpoint1.y, 10, 10);
+    // context.fillRect(endpoint2.x, endpoint2.y, 10, 10);
+
+    // context.beginPath();
+
+    // context.arcTo(endpoint1.x, endpoint1.y, endpoint2.x, endpoint2.y, radius);
+    // context.stroke();
+  };
 
   getDistance = (x0: number, y0: number, x1: number, y1: number) => {
     let xDiff = x0 - x1;
@@ -167,88 +250,6 @@ class InteractiveTriangle {
     angleC = Math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b));
 
     return angleC;
-  };
-
-  drawAngle = (
-    point: Point,
-    angle: number,
-    type: 'internal' | 'external' = 'internal'
-  ) => {
-    let context = point.context;
-    let radius = this.getShortestSide() / 4;
-
-    let allPoints = [this.pointA, this.pointB, this.pointC];
-
-    let otherPoints = allPoints.filter((arrayPoint) => arrayPoint !== point);
-    let [point1, point2] = otherPoints;
-
-    let { dx: dx1, dy: dy1 } = this.get_dx_dy(
-      point.x,
-      point.y,
-      point1.x,
-      point1.y
-    );
-
-    let { dx: dx2, dy: dy2 } = this.get_dx_dy(
-      point.x,
-      point.y,
-      point2.x,
-      point2.y
-    );
-
-    let endpoint1 = { x: point.x + dx1 * radius, y: point.y + dy1 * radius };
-    let endpoint2 = { x: point.x + dx2 * radius, y: point.y + dy2 * radius };
-
-    let angleStart = Math.atan2(dy1, dx1) + 2 * Math.PI;
-    let angleEnd = Math.atan2(dy2, dx2) + 2 * Math.PI;
-
-    if (angleEnd < angleStart) {
-      [angleStart, angleEnd] = [angleEnd, angleStart];
-    }
-
-    context.fillStyle = point.color;
-    context.strokeStyle = point.color;
-    context.lineWidth = 5;
-
-    context.beginPath();
-    if (point === this.pointA) {
-      console.log('anglestart', angleStart);
-      console.log('angleend', angleEnd);
-    }
-
-    let condition;
-
-    if ('internal') {
-      condition = true;
-    } else {
-      condition = false;
-    }
-
-    if (angleEnd - angleStart > Math.PI) {
-      context.arc(point.x, point.y, radius, angleStart, angleEnd, condition);
-    } else {
-      context.arc(point.x, point.y, radius, angleStart, angleEnd, !condition);
-    }
-
-    context.stroke();
-
-    context.lineTo(point.x, point.y);
-    context.closePath();
-
-    context.lineWidth = 3;
-
-    context.globalAlpha = this.angleTransparency;
-    context.fill();
-    context.globalAlpha = 1;
-    context.beginPath();
-
-    // context.fillRect(endpoint1.x, endpoint1.y, 10, 10);
-    // context.fillRect(endpoint2.x, endpoint2.y, 10, 10);
-
-    // context.beginPath();
-
-    // context.arcTo(endpoint1.x, endpoint1.y, endpoint2.x, endpoint2.y, radius);
-    // context.stroke();
   };
 }
 
--- pages/%C2%B0.tsx
+++ pages/%C2%B0.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styled from 'styled-components';
 import cl from '../colors';
@@ -23,15 +25,61 @@ const Degree = () => {
           />
         </Canvas1Background>
         <DegreeIntro>
-          The degree, or more formally, the{' '}
-          <span style={{ fontFamily: 'cursive' }}> ~ arc degree ~ </span>, is
-          one way to measure an angle.
+          The degree, or as it is more fancily known, the
+          <Fancy> ~ arcdegree ~ </Fancy>, is one way to measure an angle.
           <br /> <br />
           360 of these bad boys smushed together make a complete circle.
           <br /> <br />
           The symbol for the degree – ° – is, quite appropriately, a lil' baby
           circle.
         </DegreeIntro>
+        <h3>A natural question – why 360 °? Why not 100°</h3>
+        <p>
+          Degrees don't just exist in nature on there own. People had to invent
+          them . So why choose to divide circles into units of 360 pieces and
+          not choose some other more convenient number? Maybe some power of 10 —
+          100, 1000, or possibly even 1.
+          <br /> <br />
+          ¯\_(ツ)_/¯ Admittedly, sometimes in math we (as in humanity) just get
+          stuck in our ways and make life more complicated than we need to.
+          But... this isn't one of those cases! 360 degrees is much easier than
+          100 to divide without leaving the nice round, whole world, integer
+          numbers.
+          <br /> <br />
+          To understand why let's examine the multiplicative DNA of 360 and 100.
+          Here are the
+          <Fancy> prime factorizations </Fancy> for 360 and another for 100.
+        </p>
+        <div
+          style={{
+            width: '100%',
+            margin: 'auto',
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            paddingBottom: '25px',
+          }}
+        >
+          <Image src="/360_factorization.png" width={175} height={320} />
+          <Image src="/100_factorization.png" width={175} height={320} />
+        </div>
+        <P>
+          {/* Any number a, that can be divided evenly into another number b, must
+          be a product of the prime factors of b. Ufff, rewritten 3 times and
+          that last sentence is <em> still </em> an earful. If it has been
+          awhile since you've used Common Factors (GCFs and LCMs), it may make
+          more sense with a concrete example: */}
+          The reason we care about this, is that any number which can be made by
+          multiplying elements from prime factorizations can divide evenly into
+          that number with no decimals involved.{' '}
+          <Link href="https://sciencing.com/factors-number-quickly-easily-5192972.html">
+            A more detailed explanation here.
+          </Link>
+        </P>
+        <P>
+          360's prime factorization turns out to be really good at dividing
+          "evenly" into most numbers less than 20, which is most of the numbers
+          we (as in humanity) normally want to divide a circle into.
+        </P>
         <h3>Using degrees to measure triangles</h3>
         <p>
           Our eventual goal is to know everything there is to know about
@@ -73,6 +121,10 @@ const Canvas2Background = styled.div`
   background: ${cl.getHSL(cl.white)};
   /* border-top: 7px solid ${cl.getHSLA(cl.purple, 0.5)};
   border-bottom: 7px solid ${cl.getHSLA(cl.purple, 0.5)}; */
+`;
+
+const P = styled.div`
+  padding: 5px;
 `;
 
 const Fancy = styled.span`
